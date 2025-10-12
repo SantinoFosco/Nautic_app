bdc0647a6b0758a567ead7b724b38a16298a28d6
Santino Fosco <santinofosco@gmail.com>
2025-10-12 12:32:38 -0300

    Endpoint de info PopUps

---
+++ BackEnd/README.txt
@@ -0,0 +1,9 @@
+Para poder ejecutar el backEnd hay que hacer lo siguiente
+Por lineas de comandos
+
+El comando inicial puede ser py o python, depende de se pc
+
+$ py -m venv env
+$ env\Scripts\activate
+$ py -m pip install fastapi uvicorn
+$ uvicorn main:app --reload
--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,  Query
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -27,3 +27,57 @@ spots = [
 @app.get("/spots")
 async def get_spots():
     return spots
+
+# fetch(`http://127.0.0.1:8000/spots/weather_average?lat=${spot.lat}&lon=${spot.lon}&day=${day}`)
+@app.get("/spots/weather_average_mon")
+async def get_weather_average(lat: float = Query(...), lon: float = Query(...), day: int = Query(...)):
+    if day == 0:
+        return {
+            "temperature_2m": 20,
+            "wind_speed_10m": 10,
+            "precipitation": 2,
+            "wave_height": 3
+        }
+    if day == 1:
+        return {
+            "temperature_2m": 22,
+            "wind_speed_10m": 15,
+            "precipitation": 5,
+            "wave_height": 1.5
+        }
+    if day == 2:
+        return {
+            "temperature_2m": 18,
+            "wind_speed_10m": 8,
+            "precipitation": 0,
+            "wave_height": 2
+        }
+    if day == 3:
+        return {
+            "temperature_2m": 25,
+            "wind_speed_10m": 12,
+            "precipitation": 1,
+            "wave_height": 2.5
+        }
+    if day == 4:
+        return {
+            "temperature_2m": 19,
+            "wind_speed_10m": 20,
+            "precipitation": 3,
+            "wave_height": 3.5
+        }
+    if day == 5:
+        return {
+            "temperature_2m": 21,
+            "wind_speed_10m": 18,
+            "precipitation": 4,
+            "wave_height": 2.8
+        }
+    if day == 6:
+        return {
+            "temperature_2m": 23,
+            "wind_speed_10m": 14,
+            "precipitation": 2,
+            "wave_height": 1.8
+        }
+    
