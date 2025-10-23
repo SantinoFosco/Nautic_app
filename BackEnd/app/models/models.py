8150802d7493241e6a6fd4319aea37bce6e2546e
LucasMerz <lucasamerzbacher@gmail.com>
2025-10-23 10:15:34 -0300

    Tablas de dueño y negocio

--- BackEnd/app/models/models.py
+++ BackEnd/app/models/models.py
@@ -146,3 +146,62 @@ class DeporteVariable(Base):
 
     # Relaciones
     deporte = relationship("Deporte", back_populates="variables")
+    
+# ------------------------------------------------------
+# Tabla Usuario (Dueño)
+# ------------------------------------------------------
+class Usuario(Base):
+    _tablename_ = "usuario"
+
+    id_dueño = Column(Integer, Sequence("usuario_id_seq"), primary_key=True, autoincrement=True)
+    nombre = Column(String(50), nullable=False)
+    apellido = Column(String(50), nullable=False)
+    telefono = Column(String(30))
+    email = Column(String(100), unique=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    fecha_creacion = Column(TIMESTAMP(timezone=True), server_default=text("CURRENT_TIMESTAMP"))
+
+    # Relaciones
+    negocios = relationship("Negocio", backref="dueno")
+
+# ------------------------------------------------------
+# Tabla Negocio
+# ------------------------------------------------------
+
+class Negocio(Base):
+    _tablename_ = "negocio"
+
+    id_negocio = Column(Integer, Sequence("negocio_id_seq"), primary_key=True, autoincrement=True)
+    id_dueno = Column(Integer, ForeignKey("usuario.id"), nullable=False)  # Suponiendo que hay una tabla Usuario o Dueño
+    nombre_fantasia = Column(String(100), nullable=False)
+    rubro = Column(String(50))
+    sitio_web = Column(String(255))
+    telefono = Column(String(30))
+    email = Column(String(100))
+    direccion = Column(String(255))
+    coordenadas = Column(String(100))  # Ejemplo: '{"lat": -34.6, "lon": -58.4}'
+    horarios = Column(String(255))  
+    activo = Column(Boolean, default=True)
+    activo_hasta = Column(Date)
+    fecha_creacion = Column(TIMESTAMP(timezone=True), server_default=text("CURRENT_TIMESTAMP"))
+    descripcion = Column(Text)
+
+    # Relaciones
+    deportes = relationship("NegocioDeporte", backref="negocio")
+    usuario = relationship("Usuario", backref="negocios")
+
+# ------------------------------------------------------
+# Tabla intermedia Negocio_Deporte
+# ------------------------------------------------------
+class NegocioDeporte(Base):
+    _tablename_ = "negocio_deporte"
+
+    id_negocio = Column(Integer, ForeignKey("negocio.id"), nullable=False)
+    id_deporte = Column(Integer, ForeignKey("deporte.id"), nullable=False)
+    tipo_servicio = Column(String(20))
+
+    _table_args_ = (
+        PrimaryKeyConstraint("id_negocio", "id_deporte"),
+    )
+
+
